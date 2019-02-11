f97dcc71f48e4532f9bc511015cec220e5821490
saqibghaffar <saqibghaffar.heuristix@gmail.com>
2019-02-11 14:01:13 +0500

    update

--- app/controllers/sms_controller.rb
+++ app/controllers/sms_controller.rb
@@ -1,5 +1,8 @@
 class SmsController < ApplicationController
 	require 'twilio-ruby'
+	layout false, only: [:new_sms]
+	include ActionController::Live
+
 
 	def index
 		@sms_receives =  Sm.where(receiver_id: current_user.id).paginate(:page => params[:page])
@@ -19,6 +22,8 @@ class SmsController < ApplicationController
 		@sms = Sm.new
 	end 
 
+	
+
 	def create
 		@user = User.find_by_id(params[:sm][:user_number])
 		@sms = Sm.new(params_sms)
@@ -26,59 +31,63 @@ class SmsController < ApplicationController
 		@sms.receiver_id = @user.id
 		@sms.user_number = @user.mobile_number
 		if @sms.save 
+			session[:sms_id] = @sms.id
 			flash[:success] = "Sms Sent Successfully to #{@user.mobile_number}" 
-			@twilio_client = Twilio::REST::Client.new
-			begin
-				twilio = @twilio_client.messages.create(
-					from: '+14159341234',
-					to: params[:user_number],
-					body: params[:body]
-					)  
-				puts"asas #{twilio}"
-				puts twilio.to
-				return true
-			rescue Twilio::REST::RestError => error
-				@error = error
-				puts
-				return false
-			end
-			@sms_receives = Sm.where(receiver_id: current_user.id).paginate(:page => params[:page])
-			@sms_sents = Sm.where(sender_id: current_user.id).paginate(:page => params[:page])
-			if @sms.receiver_id == current_user.id 
-				render "receives.js.erb"
-			else	
-				render "sents.js.erb"
-			end 
+
+			#@twilio_client = Twilio::REST::Client.new
+			#begin
+			#	twilio = @twilio_client.messages.create(
+			#		from: '+14159341234',
+			#		to: params[:user_number],
+			#		body: params[:body]
+			#		)  
+			#	puts"asas #{twilio}"
+			#	puts twilio.to
+
+			#rescue Twilio::REST::RestError => error
+			#	@error = error
+			#	puts
+
+		#	end
+		@sms_receives = Sm.where(receiver_id: current_user.id).paginate(:page => params[:page])
+		@sms_sents = Sm.where(sender_id: current_user.id).paginate(:page => params[:page])
+		if @sms.receiver_id == current_user.id 
+			render "receives.js.erb"
+		else	
+			render "sents.js.erb"
+		end 
+	else
+		flash[:danger] = "Sms Not Sent to #{@user.mobile_number} Try Again"
+		render "new.js.erb"	
+	end
+end 
+def destroy 
+	@sms =  Sm.where(id: params[:id].split(','))
+	if @sms.delete_all 
+		flash[:success] = "Email Successfully Destroy" 
+		@sms_receives = Sm.where(receiver_id: current_user.id).paginate(:page => params[:page])
+		@sms_sents = Sm.where(sender_id: current_user.id).paginate(:page => params[:page])
+		if params[:status] != "sent"
+			render "receives.js.erb"
 		else
-			flash[:danger] = "Sms Not Sent to #{@user.mobile_number} Try Again"
-			render "new.js.erb"	
+			render "sents.js.erb"
+		end
+	else
+		flash[:danger] = "Email Not Destroy Try Again !"
+		if params[:status] != "sent"
+			render "receives.js.erb"
+		else
+			render "sents.js.erb"
 		end
 	end 
-	def destroy 
-		@sms =  Sm.where(id: params[:id].split(','))
-		if @sms.delete_all 
-			flash[:success] = "Email Successfully Destroy" 
-			@sms_receives = Sm.where(receiver_id: current_user.id).paginate(:page => params[:page])
-			@sms_sents = Sm.where(sender_id: current_user.id).paginate(:page => params[:page])
-			if params[:status] != "sent"
-				render "receives.js.erb"
-			else
-				render "sents.js.erb"
-			end
-		else
-			flash[:danger] = "Email Not Destroy Try Again !"
-			if params[:status] != "sent"
-				render "receives.js.erb"
-			else
-				render "sents.js.erb"
-			end
-		end 
-	end
+end
 
 
-	private
+private
 
-	def params_sms
-		params.require(:sm).permit(:user_number,:body,:sender_id,:receiver_id,:status)
-	end
+def params_sms
+	params.require(:sm).permit(:user_number,:body,:sender_id,:receiver_id,:status)
+end
+
+
 end
